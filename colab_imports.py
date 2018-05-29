097e20f7f82a6e524283f57a5fafa044526c5c92
CHAN,KELVIN <kechan@gmail.com>
2018-05-29 12:08:34 -0400

    add colab_imports.py

---
+++ colab_imports.py
@@ -0,0 +1,15 @@
+# https://stackoverflow.com/questions/48547660/attributeerror-module-pil-image-has-no-attribute-register-extensions
+
+# workaround 
+from PIL import Image
+def register_extension(id, extension): Image.EXTENSION[extension.lower()] = id.upper()
+Image.register_extension = register_extension
+def register_extensions(id, extensions): 
+  for extension in extensions: register_extension(id, extension)
+Image.register_extensions = register_extensions
+
+def download_data_from_gdrive(ids, filenames):
+
+    for id, filename in zip(ids, filenames):
+        uploaded = drive.CreateFile({'id': id})
+        uploaded.GetContentFile(filename)
