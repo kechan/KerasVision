e25f81afd52814dd5b9b1049fccb0f2c3e4dc487
CHAN,KELVIN <kechan@gmail.com>
2018-05-29 12:24:12 -0400

    colab import

--- colab_imports.py
+++ colab_imports.py
@@ -1,4 +1,8 @@
 # https://stackoverflow.com/questions/48547660/attributeerror-module-pil-image-has-no-attribute-register-extensions
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
 
 # workaround 
 from PIL import Image
@@ -13,3 +17,10 @@ def download_data_from_gdrive(ids, filenames):
     for id, filename in zip(ids, filenames):
         uploaded = drive.CreateFile({'id': id})
         uploaded.GetContentFile(filename)
+
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+
+
