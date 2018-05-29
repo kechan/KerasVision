5cf44d83c4e8601f41caf0a507a084e6b3bd5d03
CHAN,KELVIN <kechan@gmail.com>
2018-05-29 14:31:56 -0400

    colab import

--- colab_imports.py
+++ colab_imports.py
@@ -9,10 +9,3 @@ Image.register_extension = register_extension
 def register_extensions(id, extensions): 
   for extension in extensions: register_extension(id, extension)
 Image.register_extensions = register_extensions
-
-def download_data_from_gdrive(drive, ids, filenames):
-
-    for id, filename in zip(ids, filenames):
-        uploaded = drive.CreateFile({'id': id})
-        uploaded.GetContentFile(filename)
-
