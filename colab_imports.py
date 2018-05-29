6457041c2ea802d30ef1a0d2eb2438eca40784c1
CHAN,KELVIN <kechan@gmail.com>
2018-05-29 14:29:16 -0400

    colab import

--- colab_imports.py
+++ colab_imports.py
@@ -10,23 +10,9 @@ def register_extensions(id, extensions):
   for extension in extensions: register_extension(id, extension)
 Image.register_extensions = register_extensions
 
-def download_data_from_gdrive(ids, filenames):
+def download_data_from_gdrive(drive, ids, filenames):
 
     for id, filename in zip(ids, filenames):
         uploaded = drive.CreateFile({'id': id})
         uploaded.GetContentFile(filename)
 
-download_data_from_gdrive(['1Zdt10Q1Jn-hrq2o1mmvQ1j4DgBTxxGIq', '1FgVh2oGqH9Pr4Ze2NETyLnBTPtC0hTui', '1X6ijkgbWCzATPCJLx0rBCy5jtUkjo2KG'],
-                          ['train_224_224.hdf5.gz', 'validation_224_224.hdf5.gz', 'test_224_224.hdf5.gz'])
-
-call(["mkdir", "/content/data"])
-call(["mkdir", "/content/data/224x224_cropped_merged_heads_hdf5"])
-
-call(["gunzip", "train_224_224.hdf5.gz"])
-call(["mv", "train_224_224.hdf5", "/content/data/224x224_cropped_merged_heads_hdf5"])
-
-call(["gunzip", "validation_224_224.hdf5.gz"])
-call(["mv", "validation_224_224.hdf5", "/content/data/224x224_cropped_merged_heads_hdf5"])
-
-call(["gunzip", "test_224_224.hdf5.gz"])
-call(["mv", "test_224_224.hdf5", "/content/data/224x224_cropped_merged_heads_hdf5"])
