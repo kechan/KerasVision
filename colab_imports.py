62f93a7861b66e3d962cc287e27f4689f16d64e9
CHAN,KELVIN <kechan@gmail.com>
2018-06-03 17:21:23 -0400

    added gdrive file to id dict

--- colab_imports.py
+++ colab_imports.py
@@ -1,6 +1,13 @@
 # https://stackoverflow.com/questions/48547660/attributeerror-module-pil-image-has-no-attribute-register-extensions
 from subprocess import call
 
+google_drive_filename_id = {
+'train_224_224.hdf5.gz': '1Zdt10Q1Jn-hrq2o1mmvQ1j4DgBTxxGIqa',
+'validation_224_224.hdf5.gz': '1FgVh2oGqH9Pr4Ze2NETyLnBTPtC0hTui',
+'test_224_224.hdf5.gz': '1X6ijkgbWCzATPCJLx0rBCy5jtUkjo2KG',
+'resnet101_weights_tf.h5': '1tFWWXxctzWzcjfvuotZLYemXdw7zxDKQ',
+'keras_resnet50_fc1_weights_acc_0.9783.h5': '1Kkaay-LN7u8DYTWGm2XlHrDnKR3218CL'
+}
 
 # workaround 
 from PIL import Image
