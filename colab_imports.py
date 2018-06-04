b70c28ce2b6a3ef9fed655977b1f38766a7a15ed
CHAN,KELVIN <kechan@gmail.com>
2018-06-04 02:41:35 -0400

    add a weight to file dict

--- colab_imports.py
+++ colab_imports.py
@@ -6,7 +6,8 @@ google_drive_filename_id = {
 'validation_224_224.hdf5.gz': '1FgVh2oGqH9Pr4Ze2NETyLnBTPtC0hTui',
 'test_224_224.hdf5.gz': '1X6ijkgbWCzATPCJLx0rBCy5jtUkjo2KG',
 'resnet101_weights_tf.h5': '1tFWWXxctzWzcjfvuotZLYemXdw7zxDKQ',
-'keras_resnet50_fc1_weights_acc_0.9783.h5': '1Kkaay-LN7u8DYTWGm2XlHrDnKR3218CL'
+'keras_resnet50_fc1_weights_acc_0.9783.h5': '1Kkaay-LN7u8DYTWGm2XlHrDnKR3218CL',
+'keras_resnet101_weights_acc_0.9620.h5': '1HLY_EOY1wbzHIvD3W29uMIHq8pIx-HWe'
 }
 
 # workaround 
