6ff71c6d40619838362a9ae9081d90d6650bc8a8
CHAN,KELVIN <kechan@gmail.com>
2018-06-10 13:58:59 -0400

    plot_loss_accuracy

--- colab_imports.py
+++ colab_imports.py
@@ -30,3 +30,34 @@ Image.register_extension = register_extension
 def register_extensions(id, extensions): 
   for extension in extensions: register_extension(id, extension)
 Image.register_extensions = register_extensions
+
+import matplotlib.pyplot as plt
+
+def plot_loss_accuracy(history):
+  acc = history['acc']
+  val_acc = history['val_acc']
+  loss = history['loss']
+  val_loss = history['val_loss']
+
+  epochs = range(len(acc))
+
+  # Plotting Accuracy vs Epoch curve
+  plt.figure(figsize=(8, 8))
+
+  plt.plot(epochs, acc, 'bo', label='Training acc')
+  plt.plot(epochs, val_acc, 'b', label='Validation acc')
+  plt.title('Training and validation accuracy')
+  #plt.legend(loc='upper left')
+  plt.legend(loc='best')
+  plt.grid()
+
+
+  # Plotting Loss vs Epoch curve
+  plt.figure(figsize=(8, 8))
+
+  plt.plot(epochs, loss, 'bo', label='Training loss')
+  plt.plot(epochs, val_loss, 'b', label='Validation loss')
+  plt.title('Training and validation loss')
+  #plt.legend()
+  plt.legend(loc='best')
+  plt.grid()
