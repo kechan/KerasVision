2ff715645aa5d2924e9a63b1b6462d05efe64129
CHAN,KELVIN <kechan@gmail.com>
2018-10-13 15:39:14 -0400

    debug exif issue

--- model/convnet/resnet50_localization.py
+++ model/convnet/resnet50_localization.py
@@ -228,6 +228,7 @@ class ZoomAndFocusModel(keras.Model):
         img = PIL.Image.open(filename)
 
 	# take care of jpeg img orientation issue of PIL.Image.open
+	print(type(img))
 	if type(img) == PIL.JpegImagePlugin.JpegImageFile:
             exif = dict(img._getexif().items())
             inv_ExifTags = dict([(property, idx) for idx, property in ExifTags.TAGS.items()])	
